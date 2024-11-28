2dab63506e88054b2fae3c7b715f4bae8e111bdb
Pappu9931 <134928138+Pappu9931@users.noreply.github.com>
2024-11-28 20:03:43 +0530

    final result

---
+++ src/components/custom/Footer.jsx
@@ -0,0 +1,15 @@
+
+
+
+function Footer() {
+
+  return (
+    <div className="footer min-h-32 w-full flex text-muted-foreground items-center justify-center p-4">
+      <h1>
+      Thank you
+      </h1>
+    </div>
+  );
+}
+
+export default Footer;
